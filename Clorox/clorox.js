69ce03996d4613adbe77916281de4296c968b451
Juan <juanlb1844@gmail.com>
2018-04-11 23:37:29 -0500

    cambios mínimos

--- Clorox/clorox.js
+++ Clorox/clorox.js
@@ -1,36 +1,50 @@
-$(document).ready(
-		function() { 
+$(document).ready(function(){
 
-		class initBeer {
-			constructor(nameSpace, pixels) {
-				this.nameSpace = nameSpace; 
-				this.pixels = pixels; 
+	function clorox(cloroConf, selector, proportions) {
+		this.config = cloroConf; 
+		this.selector = selector; 
+		this.proportions = proportions; 
+		this.initView = function() {
+				for(i in this.config) {
+						console.log($(this.selector).html()); 
+						$(this.selector).css('width', this.config.width)
+										.css('border', this.config.border)
+										.css('min-height', this.config.minHeight)
+										.css('position', 'absolute'); 
+				}
+		}; 
+		this.create = function() {
+			for( i = 0; i < this.proportions; i++) {
+					element = $('<div>'); 
+					element.append('<h1>X</h1>'); 
+					element.css('float', 'left').css('width', '9.2%').css('background-color', 'black')
+					.css('margin', '4px'); 
+					$(element).addClass('pixel'+i); 
+					$(this.selector).append(element); 
 			}
-			get getName() {
-				return this.nameSpace; 
-			}
+		} 
+	}
 
-		}
+	var cloroConf_1 = {
+		'width' : '80%', 
+		'minHeight' : '400px', 
+		'position' : 'absolute', 
+		'border'   : '10px solid black'
+	}
+	var A1 = new clorox(cloroConf_1, "#clorox", 60); 
+	A1.initView(); 
+	A1.create();
 
-		//functions.. && 
-
-		const clorox = new initBeer('clorox', 20);  
-
-		var stylesDie = {
-			dimensions : function (width, height) {
-							var cad = "#"+(clorox.getName); 
-							$(cad).css('order', '10px solid black')
-							.css('height', 'auto')
-							.css('width', '70%')
-							.css('margin', 'auto')
-							.css('position', 'absolute');
-
-							for(var i = 0; i < 40; i++) {
-									$(cad).append('<div style="background-color: black; float:left; width:9%; margin:2px; border:1px solid green;" class="pixel'+i+'"><h1>X</h1></div>'); 
-							}
-			}
-		}
-		stylesDie.dimensions(); 
+	var cloroConf_2 = {
+		'width' : '80%', 
+		'minHeight' : '400px', 
+		'position' : 'absolute', 
+		'border'   : '10px solid black'
+	}
+	var A2 = new clorox(cloroConf_2, "#clorox2", 60); 
+	A2.initView(); 
+	A2.create();  
 
 
-});
+
+}); 
