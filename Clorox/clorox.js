b88d1f8a8b6c0f1a2ca8af13dc759912f8ea0f4c
Juan <juanlb1844@gmail.com>
2018-04-11 20:59:42 -0500

    cambios menores

--- Clorox/clorox.js
+++ Clorox/clorox.js
@@ -9,25 +9,25 @@ $(document).ready(
 			get getName() {
 				return this.nameSpace; 
 			}
+
 		}
 
 		//functions.. && 
 
-		const clorox = new initBeer('clorox', 20); 
-		console.log(clorox.getName); 
+		const clorox = new initBeer('clorox', 20);  
 
 		var stylesDie = {
 			dimensions : function (width, height) {
-				var cad = "#"+(clorox.getName); 
-				$(cad).css('border', '10px solid black')
-					.css('height', 'auto')
-					.css('width', '70%')
-					.css('margin', 'auto')
-					.css('position', 'absolute');
+							var cad = "#"+(clorox.getName); 
+							$(cad).css('order', '10px solid black')
+							.css('height', 'auto')
+							.css('width', '70%')
+							.css('margin', 'auto')
+							.css('position', 'absolute');
 
-				for(var i = 0; i < 40; i++) {
-					$(cad).append('<div style="float:left; width:9%; margin:2px; border:1px solid green;" class="pixel'+i+'"><h1>X</h1></div>'); 
-				}
+							for(var i = 0; i < 40; i++) {
+									$(cad).append('<div style="background-color: black; float:left; width:9%; margin:2px; border:1px solid green;" class="pixel'+i+'"><h1>X</h1></div>'); 
+							}
 			}
 		}
 		stylesDie.dimensions(); 
